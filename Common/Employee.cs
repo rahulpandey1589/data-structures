75d80cd0749df9e9bbde95e625b595d1f8411b22
Rahul Pandey <rahulpandey862@gmail.com>
2021-07-11 11:45:29 +0530

    Intial Checkin

---
+++ Common/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace DataStructures.Common
+{
+    public class Employee
+    {
+        public Employee(string firstName,
+            string lastName,
+            string gender)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Gender = gender;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+    }
+}
